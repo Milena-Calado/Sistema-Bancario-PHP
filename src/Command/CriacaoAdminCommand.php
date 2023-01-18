f94c6d006ba49553c4a82accf6605965bbd95016
Milena Calado <milenacalado@hotmail.com>
2023-01-18 18:49:24 -0300

    Continuando a configurar

--- src/Command/CriacaoAdminCommand.php
+++ src/Command/CriacaoAdminCommand.php
@@ -2,42 +2,53 @@
 
 namespace App\Command;
 
-use Symfony\Component\Console\Attribute\AsCommand;
+use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 #[AsCommand(
-    name: 'CriacaoAdmin',
-    description: 'Criando o administrador do sistema',
+    name: 'app:create-user',
+    description: 'Cria um usuário no banco de dados',
 )]
 class CriacaoAdminCommand extends Command
 {
+    public function __construct(
+        private UserPasswordHasherInterface $hasher,
+        private UserRepository $users
+    )
+    {
+        parent::__construct();
+    }
+
+    
     protected function configure(): void
     {
         $this
-            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
-            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+            ->addArgument('email', InputArgument::REQUIRED, 'E-mail')
+            ->addArgument('password', InputArgument::REQUIRED, 'Password')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
-        $arg1 = $input->getArgument('arg1');
+        $email = $input->getArgument('email');
+        $password = $input->getArgument('password');
 
-        if ($arg1) {
-            $io->note(sprintf('You passed an argument: %s', $arg1));
-        }
+        $user = new User(); 
+        $user->setEmail($email);
+        $user->setPassword(
+            $this->hasher->hashPassword($user, $password)
+        );
+        $this->users->save($user, true);
 
-        if ($input->getOption('option1')) {
-            // ...
-        }
-
-        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+        $io->success(sprintf('Usuário %s criado!', $email));
 
         return Command::SUCCESS;
     }
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,35 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Agencia;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(
+        private UserPasswordHasherInterface $hasher
+    )
+    {}
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $user1 = new User(); 
+        $user1->setEmail('mcsa@cin.ufpe.br');
+        $user1->setPassword(
+            $this->hasher->hashPassword($user1, '123456')
+        );
+        $manager->persist($user1);
 
+        $user2 = new User(); 
+        $user2->setEmail('milena@cin.ufpe.br');
+        $user2->setPassword(
+            $this->hasher->hashPassword($user2, '123456')
+        );
+        $manager->persist($user2);
+        
         $manager->flush();
     }
 }
